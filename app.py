9bf234eb8c61d8f8464baf0bac47f8e212e3191b
mastercam123 <k4mbingoce@yahoo.com>
2025-03-06 14:57:56 +0100

    Updated the python app.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     image_version = os.getenv("IMAGE_VERSION", "unknown")
-    return "🚀 Hello from Flask in Kargo for 1st time ! 🚀 Version: " + image_version
+    return "🚀 Hello there, this is from Flask in Kargo for 1st time ! 🚀 Version: " + image_version
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
